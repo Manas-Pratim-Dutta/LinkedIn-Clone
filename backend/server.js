79bdc25f833a07dc09c0b846b3af47b35bd693c1
Manas Pratim Dutta <manaspratimdutta1999@gmail.com>
2024-10-12 23:43:55 +0530

    Made users route

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js"
+import userRoutes from "./routes/users.route.js"
 import { connectDB } from "./database/db.js";
 
 dotenv.config();
@@ -10,8 +12,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // parse JSON request bodies.
+app.use(cookieParser());
 
 app.use("/api/v1/auth", authRoutes)
+app.use("/api/v1/users", userRoutes)
+
 
 app.listen(PORT, () => {
     console.log(`Server listen's at ${PORT}`);
