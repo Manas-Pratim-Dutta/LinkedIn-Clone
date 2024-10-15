74d9fb58f817efda31ac6a4a0c5090aa86c0215c
Manas Pratim Dutta <manaspratimdutta1999@gmail.com>
2024-10-15 23:56:27 +0530

    Users publicrprofile,sugestions

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,11 +1,63 @@
 import User from "../models/user.model.js";
 
-export const getSuggestedConnection = async(req,res) =>{
+export const getSuggestedConnection = async (req, res) => {
     try {
         const currentUser = await User.findById(req.user._id).select("connections");
 
         // find users who are not already connected and also do not recommend our own profile.
+        const suggestedUser = await User.find({
+            _id: {
+                $ne: req.user._id,
+                $nim: currentUser.connections
+            }
+        })
+            .select("name username profilePicture headline")
+            .limit(4);
+
+
+        res.json(suggestedUser);
     } catch (error) {
-        
+        console.error("Error in getSuggestedConnections controller:", error);
+        res.status(500).json({ message: "Server error" });
+    }
+}
+
+export const getPublicProfile = async (req, res) => {
+    try {
+        const user = await User.findOne({ username: req.params.username }).select("-password")
+
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+    } catch (error) {
+        console.error("Error in getPublicProfile controller:", error);
+        res.status(500).json({ message: "Server error" });
+    }
+}
+
+export const updateProfile = async (req, res) => {
+    try {
+        const allowedFields = [
+            "name",
+            "username",
+            "headline",
+            "about",
+            "location",
+            "profilePicture",
+            "bannerImg",
+            "skills",
+            "experience",
+            "education",
+        ];
+
+        const updatedData = {};
+
+        for(const field of allowedFields){
+            if(req.body[field]){
+                updatedData[field] = req.body[field];
+            }
+        }
+    } catch (error) {
+
     }
 }
--- backend/routes/users.route.js
+++ backend/routes/users.route.js
@@ -1,10 +1,13 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { getSuggestedConnection } from "../controllers/user.controller.js";
+import { getSuggestedConnection, getPublicProfile, updateProfile } from "../controllers/user.controller.js";
 
 
 const router = express.Router();
 
-router.get("/suggestions", protectRoute, getSuggestedConnection)
+router.get("/suggestions", protectRoute, getSuggestedConnection);
+router.get("/:username", protectRoute, getPublicProfile);
+
+router.put("/profile", protectRoute, updateProfile);
 
 export default router;
