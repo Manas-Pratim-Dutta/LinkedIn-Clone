90f8269c06caed74fa7cd096b04810beebe38067
Manas Pratim Dutta <manaspratimdutta1999@gmail.com>
2024-10-21 01:44:31 +0530

    notofication routes created

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js"
 import userRoutes from "./routes/users.route.js"
 import postRoutes from "./routes/post.route.js"
+import notificationRoutes from "./routes/notification.route.js"
 import { connectDB } from "./database/db.js";
 
 dotenv.config();
@@ -18,6 +19,7 @@ app.use(cookieParser());
 app.use("/api/v1/auth", authRoutes)
 app.use("/api/v1/users", userRoutes)
 app.use("/api/v1/posts", postRoutes)
+app.use("/api/v1/notifications", notificationRoutes)
 
 
 app.listen(PORT, () => {
