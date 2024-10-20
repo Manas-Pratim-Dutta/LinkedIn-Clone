90366b53d21649bf9eb032e2969fdc594dda9d88
Manas Pratim Dutta <manaspratimdutta1999@gmail.com>
2024-10-20 23:59:32 +0530

    updated

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -1,5 +1,6 @@
 import cloudinary from "../database/cloudinary.js";
 import Post from "../models/post.model.js";
+import Notification from "../models/notification.model.js";
 
 export const getFeedPosts = async (req, res) => {
     try {
@@ -13,7 +14,7 @@ export const getFeedPosts = async (req, res) => {
         console.error("Error in getFeedPosts controller:", error);
         res.status(500).json({ message: "Server error" });
     }
-}
+};
 
 export const createPost = async (req, res) => {
     try {
@@ -40,32 +41,66 @@ export const createPost = async (req, res) => {
         console.error("Error in createPost controller:", error);
         res.status(500).json({ message: "Server error" });
     }
-}
+};
 
-export const deletePost = async (req, res) =>{
+export const deletePost = async (req, res) => {
     try {
         const postId = req.params.id;
         const userId = req.user._id;
 
         const post = await Post.findById(postId);
 
-        if(!post){
-            return res.status(404).json({message: "Post not found"});
+        if (!post) {
+            return res.status(404).json({ message: "Post not found" });
         }
 
         // check if the current user is the author of the post
-		if (post.author.toString() !== userId.toString()) {
-			return res.status(403).json({ message: "You are not authorized to delete this post" });
-		}
+        if (post.author.toString() !== userId.toString()) {
+            return res.status(403).json({ message: "You are not authorized to delete this post" });
+        }
 
-        if(post.image){
-            //todo: do this later
+        if (post.image) {
+            await cloudinary.uploader.destroy(post.image.split("/").pop().split(".")[0]);
         }
 
         await Post.findByIdAndDelete(postId);
         res.status(200).json({ message: "Post deleted successfully" });
     } catch (error) {
         console.log("Error in delete post controller", error.message);
-		res.status(500).json({ message: "Server error" });
+        res.status(500).json({ message: "Server error" });
+    }
+};
+
+export const getPostById = async (req, res) => {
+    try {
+        const postId = req.params.id;
+        const post = await Post.findById(postId)
+            .populate("author", "name username profilePicture headline")
+            .populate("comments.user", "name profilePicture username headline");
+
+        res.status(200).json(post);
+    } catch (error) {
+        console.error("Error in getPostById controller:", error);
+        res.status(500).json({ message: "Server error" });
+    }
+};
+
+export const createComment = async (req, res) => {
+    try {
+        const postId = req.params.id;
+        const { content } = req.body;
+
+        const post = await Post.findByIdAndUpdate(postId, {
+            $push: { comments: { user: req.user._id, content } },
+        },
+            { new: true }
+        ).populate("author", "name email username headline profilePicture");
+
+        // create a notification if the comment owner is not the post owner
+        if(post.author.toString() !== req.user._id.toString()){
+            const newNotification = new Notification
+        }
+    } catch (error) {
+
     }
 }
