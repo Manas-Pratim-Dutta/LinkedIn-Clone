3762a9c183f3b58c91d426d30833132ea0fd8f1e
Manas Pratim Dutta <manaspratimdutta1999@gmail.com>
2024-10-21 01:14:00 +0530

    comment, like post functionality done

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -1,6 +1,7 @@
 import cloudinary from "../database/cloudinary.js";
 import Post from "../models/post.model.js";
 import Notification from "../models/notification.model.js";
+import { sendCommentNotificationEmail } from "../emails/emailHandlers.js";
 
 export const getFeedPosts = async (req, res) => {
     try {
@@ -97,10 +98,63 @@ export const createComment = async (req, res) => {
         ).populate("author", "name email username headline profilePicture");
 
         // create a notification if the comment owner is not the post owner
-        if(post.author.toString() !== req.user._id.toString()){
-            const newNotification = new Notification
+        if (post.author.toString() !== req.user._id.toString()) {
+            const newNotification = new Notification({
+                recipient: post.author,
+                type: "comment",
+                relatedUser: req.user._id,
+                relatedPost: postId,
+            })
+            await newNotification.save(post)
+
+            try {
+                const postUrl = process.env.CLIENT_URL + "/post/" + postId
+                await sendCommentNotificationEmail(
+                    post.author.email,
+                    post.author.name,
+                    req.user.name,
+                    postUrl,
+                    content
+                )
+            } catch (error) {
+                console.log("Error in sending comment notification email:", error);
+            }
         }
+
+        res.status(200).json(post)
     } catch (error) {
 
+    }
+}
+
+export const likePost = async (req, res) => {
+    try {
+        const postId = req.params.id;
+        const post = await Post.findById(postId);
+        const userId = req.user._id;
+
+        if (post.likes.includes(userId)) {
+            // Unlike the post--> just filter out the current user out
+            post.likes = post.likes.filter(id => id.toString() !== userId.toString());
+        } else {
+            // Like the post
+            post.likes.push(userId);
+            // create a notification if the post owner is not the user who liked
+            if (post.author.toString() !== userId.toString()) {
+                const newNotification = new Notification({
+                    recipient: post.author,
+                    type: "like",
+                    relatedUser: userId,
+                    relatedPost: postId,
+                })
+                await newNotification.save(post)
+            }
+
+        }
+        await post.save();
+        res.status(200).json(post);
+    } catch (error) {
+        console.error("Error in likePost controller:", error);
+        res.status(500).json({ message: "Server error" });
     }
 }
