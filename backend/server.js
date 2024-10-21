9b844434ffe41d3683dd25627f71612585f3a0ca
Manas Pratim Dutta <manaspratimdutta1999@gmail.com>
2024-10-22 01:20:27 +0530

    Notification,Connection api's completed

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import authRoutes from "./routes/auth.route.js"
 import userRoutes from "./routes/users.route.js"
 import postRoutes from "./routes/post.route.js"
 import notificationRoutes from "./routes/notification.route.js"
+import connectionRoutes from "./routes/connection.route.js"
 import { connectDB } from "./database/db.js";
 
 dotenv.config();
@@ -13,13 +14,14 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json()); // parse JSON request bodies.
+app.use(express.json({limit:"5mb"})); // parse JSON request bodies.
 app.use(cookieParser());
 
 app.use("/api/v1/auth", authRoutes)
 app.use("/api/v1/users", userRoutes)
 app.use("/api/v1/posts", postRoutes)
 app.use("/api/v1/notifications", notificationRoutes)
+app.use("/api/v1/connections", connectionRoutes)
 
 
 app.listen(PORT, () => {
