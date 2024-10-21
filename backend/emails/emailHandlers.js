eaf76af489d5a1f71b66b01fda67546904815eb7
Manas Pratim Dutta <manaspratimdutta1999@gmail.com>
2024-10-22 01:21:07 +0530

    Connection Accepted Email functionality added

--- backend/emails/emailHandlers.js
+++ backend/emails/emailHandlers.js
@@ -1,5 +1,5 @@
 import { mailtrapClient, sender } from "../database/mailtrap.js";
-import { createCommentNotificationEmailTemplate, createWelcomeEmailTemplate } from "./emailTemplates.js";
+import { createCommentNotificationEmailTemplate, createConnectionAcceptedEmailTemplate, createWelcomeEmailTemplate } from "./emailTemplates.js";
 
 export const sendWelcomeEmail = async (email, name, profileUrl) => {
     const recipient = [{ email }];
@@ -46,4 +46,17 @@ export const sendCommentNotificationEmail = async (
     } catch (error) {
         throw error;
     }
+};
+
+export const sendConnectionAcceptedEmail = async (senderEmail, senderName, recipientName, profileUrl) => {
+    const recipient = [{ email: senderEmail }];
+    try {
+        const response = await mailtrapClient.send({
+            from: sender,
+            to: recipient,
+            subject: `${recipientName} accepted your connection request`,
+            html: createConnectionAcceptedEmailTemplate(senderName, recipientName, profileUrl),
+            category: "connection_accepted",
+        });
+    } catch (error) {}
 }
