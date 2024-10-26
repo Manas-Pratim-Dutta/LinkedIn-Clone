8d17e24fadbaec3de6630a490b4b7fa61835d025
Manas Pratim Dutta <manaspratimdutta1999@gmail.com>
2024-10-27 00:25:46 +0530

    typo error fixed

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 app.use(
     cors({
         origin: "http://localhost:5173",
-        creadentials: true, //-> allowed them to sent cookies with response
+        credentials: true, //-> allowed them to sent cookies with response
     })
 );
 
